3817549eab008bb581ec9ad595eed78ca93a3a7a
Watchanon Numnam <jiramot@gmail.com>
2018-01-24 13:05:28 +0700

    Add simple user controller

--- src/main/java/com/jiramot/auth/controller/UserController.java
+++ src/main/java/com/jiramot/auth/controller/UserController.java
@@ -1,4 +1,15 @@
 package com.jiramot.auth.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
+  @GetMapping
+  public @ResponseBody
+  String getUser() {
+    return "{\"users\":[{\"firstname\":\"Richard\", \"lastname\":\"Feynman\"}," +
+        "{\"firstname\":\"Marie\",\"lastname\":\"Curie\"}]}";
+  }
 }
