58ddd92bcbf9e4fc6b43474adb92bc50ad534e03
Watchanon Numnam <jiramot@gmail.com>
2018-01-24 13:05:13 +0700

    Add simple hello controller

--- src/main/java/com/jiramot/auth/controller/HelloController.java
+++ src/main/java/com/jiramot/auth/controller/HelloController.java
@@ -1,4 +1,13 @@
 package com.jiramot.auth.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
+
+  @GetMapping("/")
+  String hello() {
+    return "hello world";
+  }
 }
